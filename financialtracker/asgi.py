81c4966f506765a72c16478bb41b7b9012e42c55
khanjan04 <khanjan.varma@xecurify.com>
2024-10-25 12:10:19 +0530

    Project Name changed

--- financialtracker/asgi.py
+++ financialtracker/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for perfin project.
+ASGI config for financialtracker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perfin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'financialtracker.settings')
 
 application = get_asgi_application()
